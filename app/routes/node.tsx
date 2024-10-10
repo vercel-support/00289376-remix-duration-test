f8c774c3f58f3fa93643ec095f8b532b11e09f7f
Nick Vigier <nvigier46@gmail.com>
2024-10-10 08:44:53 +0100

    test maxDuration config

--- app/routes/node.tsx
+++ app/routes/node.tsx
@@ -1,10 +1,16 @@
-import type { MetaFunction } from "@vercel/remix";
+import type { MetaFunction } from '@vercel/remix';
 
-export const meta: MetaFunction = () => [{ title: "Remix@Edge | New Remix App" }];
+export const meta: MetaFunction = () => [
+  { title: 'Remix@Edge | New Remix App' },
+];
+
+export const config = {
+  maxDuration: 60,
+};
 
 export default function Node() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Welcome to Remix@Node</h1>
     </div>
   );
